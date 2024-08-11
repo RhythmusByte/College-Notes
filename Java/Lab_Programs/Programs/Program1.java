35d6c45a2ca97ffabd0c19a25720575ae4e78bb2
Akhil Mahesh <akhilmahesh@proton.me>
2024-08-11 18:47:57 +0530

    Create Program1.java

---
+++ Java/Lab_Programs/Programs/Program1.java
@@ -0,0 +1,26 @@
+class Program1 {
+	public static void main(String[] args) {
+		if(args.length != 2) {
+			System.err.print("Enter 2 numbers as arguments!!!");
+			System.exit(0);
+		}
+
+		String i = args[0];
+		String j = args[1];
+
+		int a = Integer.parseInt(i);
+		int b = Integer.parseInt(j);
+
+		int sum = a + b;
+		int product = a * b;
+		int quotient = a / b;
+		int difference = a - b;
+		int remainder = a % b;
+
+		System.out.println("Sum: " + sum);
+		System.out.println("Product: " + product);
+		System.out.println("Quotient: " + quotient);
+		System.out.println("Difference: " + difference);
+		System.out.println("Remainder: " + remainder);
+	}
+}
