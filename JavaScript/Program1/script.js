36f4dd1844112a6e2ab3cc180bfba5c7891e8867
Akhil Mahesh <akhilmahesh@proton.me>
2024-08-13 19:23:27 +0530

    Create script.js

---
+++ JavaScript/Program1/script.js
@@ -0,0 +1,28 @@
+function checkNumberType() {
+    var number = parseInt(prompt("Enter a number:"));
+    
+    function sumOfDivisors(num) {
+        let sum = 0;
+        for (let i = 1; i <= num / 2; i++) {
+            if (num % i === 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    var sum = sumOfDivisors(number);
+
+    let result;
+    if (sum === number) {
+        result = number + " is a perfect number.";
+    } else if (sum > number) {
+        result = number + " is an abundant number.";
+    } else {
+        result = number + " is a deficient number.";
+    }
+
+    alert(result);
+}
+
+checkNumberType();
