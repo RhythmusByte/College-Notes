cbcddb678b87716afe2ad7ade7515fb70dbbee1a
Akhil Mahesh <akhilmahesh@proton.me>
2024-08-14 11:35:24 +0530

    Add files via upload

---
+++ Java/Lab_Programs/Programs/Program4.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+interface Volume {
+    double PI = Math.PI;
+    void readData();
+    void dispVolume();
+}
+
+class Sphere implements Volume {
+    private double radius;
+    private double volume;
+
+    public void readData() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the radius of the sphere: ");
+        radius = scan.nextDouble();
+    }
+
+    public void dispVolume() {
+        volume = (4.0 / 3.0) * PI * Math.pow(radius, 3);
+        System.out.println("Volume of the sphere is " + volume);
+        System.out.println("\n\n--------------------------\n\n");
+    }
+}
+
+class Cylinder implements Volume {
+    private double radius;
+    private double volume;
+    private double height;
+
+    public void readData() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the radius of the cylinder: ");
+        radius = scan.nextDouble();
+        System.out.print("\nEnter the height of the cylinder: ");
+        height = scan.nextDouble();
+    }
+
+    public void dispVolume() {
+        double volume = PI * Math.pow(radius, 2) * height;
+        System.out.println("Volume of the cylinder is " + volume);
+    }
+}
+
+public class Program4 {
+    public static void main(String[] args) {
+        Volume sphere = new Sphere();
+        sphere.readData();
+        sphere.dispVolume();
+
+        Volume cylinder = new Cylinder();
+        cylinder.readData();
+        cylinder.dispVolume();
+    } 
+}
