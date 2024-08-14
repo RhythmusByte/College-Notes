1b2d1afea389e248897b137f753fd9b0d8d2266d
Akhil Mahesh <akhilmahesh@proton.me>
2024-08-14 10:55:14 +0530

    Add: Program 3

---
+++ Java/Lab_Programs/Programs/Program3.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Program3 {
+    public int[] array;
+
+    public Program3(int[] array) {
+        this.array = array;
+    }
+
+    public void sortArray() {
+        Arrays.sort(array);
+    }
+
+    public int smallestNumber() {
+        return array[0];
+    }
+
+    public int largestNumber() {
+        return array[array.length -1];
+    }
+
+    public int secondLargest() {
+        return array[array.length -2];
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int size = scan.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements:");
+
+        for(int i = 0; i < size; i++) {
+            array[i] = scan.nextInt();
+        }
+
+        Program3 myarr = new Program3(array);
+
+        myarr.sortArray();
+        System.out.println("Smallest number in the array is  " + myarr.smallestNumber());
+        System.out.println("Largest number in this array is " + myarr.largestNumber());
+        System.out.println("Second largest number in this array is " + myarr.secondLargest());
+    }
+}
