195fe0e25635b89041583e1db19d305b099690d5
RhythmusByte <akhilmahesh@proton.me>
2024-08-18 19:44:19 +0530

    Program 4 Update

--- Java/Lab_Programs/Programs/Program4.java
+++ Java/Lab_Programs/Programs/Program4.java
@@ -1,4 +1,4 @@
-/*
+/*k
 Create an interface volume with member variable 
 pi and methods readdata () and dispvolume (). 
 Create two classes sphere and cylinder to 
@@ -6,7 +6,7 @@ implement this interface.
 Write a Java program to find the volume.
 */
 
-import java.util.*;
+import java.util.Scanner;
 
 interface Volume {
     double PI = Math.PI;
@@ -16,34 +16,31 @@ interface Volume {
 
 class Sphere implements Volume {
     private double radius;
-    private double volume;
-
+    
     public void readData() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the radius of the sphere: ");
         radius = scan.nextDouble();
     }
-
+    
     public void dispVolume() {
-        volume = (4.0 / 3.0) * PI * Math.pow(radius, 3);
+        double volume = (4.0 / 3.0) * PI * Math.pow(radius, 3);
         System.out.println("Volume of the sphere is " + volume);
-        System.out.println("\n\n--------------------------\n\n");
     }
 }
 
 class Cylinder implements Volume {
     private double radius;
-    private double volume;
     private double height;
-
+    
     public void readData() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the radius of the cylinder: ");
         radius = scan.nextDouble();
-        System.out.print("\nEnter the height of the cylinder: ");
+        System.out.print("Enter the height of the cylinder: ");
         height = scan.nextDouble();
     }
-
+    
     public void dispVolume() {
         double volume = PI * Math.pow(radius, 2) * height;
         System.out.println("Volume of the cylinder is " + volume);
@@ -51,13 +48,39 @@ class Cylinder implements Volume {
 }
 
 public class Program4 {
-    public static void main(String[] args) {
-        Volume sphere = new Sphere();
-        sphere.readData();
-        sphere.dispVolume();
+    private static Scanner scan = new Scanner(System.in);
 
-        Volume cylinder = new Cylinder();
-        cylinder.readData();
-        cylinder.dispVolume();
-    } 
+    public static void main(String[] args) {
+        int choice;
+        do {
+            displayMenu();
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    Volume sphere = new Sphere();
+                    sphere.readData();
+                    sphere.dispVolume();
+                    break;
+                case 2:
+                    Volume cylinder = new Cylinder();
+                    cylinder.readData();
+                    cylinder.dispVolume();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 3);
+        scan.close();
+    }
+
+    private static void displayMenu() {
+        System.out.println("\nFind the Volume\n---------------\n");
+        System.out.println("\t1. Sphere");
+        System.out.println("\t2. Cylinder");
+        System.out.println("\t3. Exit");
+        System.out.print("\nEnter your choice: ");
+    }
 }
