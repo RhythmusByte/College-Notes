b233b3eb1bdb6114fe6ecdd6e6896cc714ca7336
Akhil Mahesh <akhilmahesh@proton.me>
2025-01-27 13:02:06 +0530

    File Updated

--- Java-Lab/Program-5/MergeArrays.java
+++ Java-Lab/Program-5/MergeArrays.java
@@ -1,32 +1,45 @@
 import java.util.Arrays;
 
 public class MergeArrays {
-    public static void main(String[] args) {
-    
-        int[] array1 = {1, 3, 5, 7};
-  int[] array2 = {2, 4, 6, 8};
+    private int[] array1 = {19, 32, 45, 27};
+    private int[] array2 = {23, 14, 60, 81};
+    private int[] mergedArray = new int[array1.length + array2.length];
 
-        int[] mergedArray = mergeArrays(array1, array2);
+    public MergeArrays() {
+        mergeArrays();
+        printMessage();
+    }
 
+    private void mergeArrays() {
+        int i = 0, j = 0, k = 0;
+
+        // Merge the arrays while keeping the order
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                mergedArray[k++] = array1[i++];
+            } else {
+                mergedArray[k++] = array2[j++];
+            }
+        }
+
+        // If there are any elements left in array1
+        while (i < array1.length) {
+            mergedArray[k++] = array1[i++];
+        }
+
+        // If there are any elements left in array2
+        while (j < array2.length) {
+            mergedArray[k++] = array2[j++];
+        }
+    }
+
+    private void printMessage() {
         System.out.println("Array 1: " + Arrays.toString(array1));
         System.out.println("Array 2: " + Arrays.toString(array2));
-
         System.out.println("Merged Array: " + Arrays.toString(mergedArray));
     }
 
-    public static int[] mergeArrays(int[] array1, int[] array2) {
-
-        int[] mergedArray = new int[array1.length + array2.length];
-
-        for (int i = 0; i < array1.length; i++) {
-            mergedArray[i] = array1[i];
-        }
-
-        for (int i = 0; i < array2.length; i++) {
-            mergedArray[array1.length + i] = array2[i];
-        }
-        Arrays.sort(mergedArray);
-
-        return mergedArray;
+    public static void main(String[] args) {
+        new MergeArrays();
     }
 }
