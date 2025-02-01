f0c7a8d245b0299fb765cf3dcf6a6f3715c3f9a1
Akhil Mahesh <akhilmahesh@proton.me>
2025-02-01 10:53:08 +0530

    Refactor: Removed $ sign in syntax

--- Shell/Program-6.sh
+++ Shell/Program-6.sh
@@ -6,9 +6,9 @@ read num
 sum=0
 
 while [[ $num -gt 0 ]]; do
-  digit=$(( $num % 10 ))
-  sum=$(( $sum + $digit ))
-  num=$(( $num / 10 ))
+  digit=$(( num % 10 ))
+  sum=$(( sum + digit ))
+  num=$(( num / 10 ))
 done
 
-echo "Sum of the digit is $sum"
+echo "Sum: $sum"
