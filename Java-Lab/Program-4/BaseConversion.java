535a4ed67671204f1b675e55f3c95bc7f1e36167
Akhil Mahesh <akhilmahesh@proton.me>
2025-03-05 15:11:22 +0530

    refactor(java-lab): change in implementation of OOP concept

--- Java-Lab/Program-4/BaseConversion.java
+++ Java-Lab/Program-4/BaseConversion.java
@@ -2,33 +2,29 @@ import java.util.Scanner;
 
 public class BaseConversion {
   private static final Scanner SCANNER = new Scanner(System.in);
-  private int num;
   
-  public BaseConversion() {
-    readValue();
-    toBinary();
-    toOctal();
-    toHexadecimal();
-  }
-
-  private void readValue() {
+  private int readValue() {
     System.out.print("Enter a number: ");
-    num = SCANNER.nextInt();
+    return SCANNER.nextInt();
   }
 
-  private void toBinary() {
+  private void toBinary(int num) {
     System.out.println("Binary: " + Integer.toBinaryString(num));
   }
 
-  private void toOctal() {
+  private void toOctal(int num) {
     System.out.println("Octal: " + Integer.toOctalString(num));
   }
 
-  private void toHexadecimal() {
+  private void toHexadecimal(int num) {
     System.out.println("Hexadecimal: " + Integer.toHexString(num).toUpperCase());
   }
 
   public static void main(String[] args) {
-    new BaseConversion();
+    BaseConversion bc = new BaseConversion();
+    int num = bc.readValue();
+    bc.toBinary(num);
+    bc.toOctal(num);
+    bc.toHexadecimal(num);
   }
 }
